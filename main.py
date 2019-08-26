796d92b0e0810dae101f8a8681d60308b08d3351
Alejandro Hernández Gómez <alejandro.hernandez@jscda.com>
2019-08-26 14:59:13 +0200

    add a ganadora...

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from lib.ApiData import ApiData
 from lib.InstrumentsManager import InstrumentsManager
 
@@ -14,161 +13,171 @@ import json
 from datetime import datetime
 from time import sleep
 
-
 apiData = ApiData()
 instrumentsManager = InstrumentsManager({})
 errorsManagement = Client('https://7932a7da676c4962895957059416bd7d:da9a1669ee724bb2b61cf7b47b430ccc@sentry.io/154029')
 
-#Strategies
+# Strategies
 granularity = "M5"
 ichimoku = Ichimoku.Ichimoku(granularity)
 
 DEBUG = True;
 
+
 def main():
-	#instruments_list = "AUD_CHF,CAD_CHF,NZD_USD,GBP_USD,AUD_USD,EUR_USD,EUR_GBP";
-	instruments_list = "";
-	instrumentsManager.GetTradeableInstruments();
-	for inst in instrumentsManager.instruments:
-		instruments_list +=  inst + ",";
+    # instruments_list = "AUD_CHF,CAD_CHF,NZD_USD,GBP_USD,AUD_USD,EUR_USD,EUR_GBP";
+    instruments_list = "";
+    instrumentsManager.GetTradeableInstruments();
+    for inst in instrumentsManager.instruments:
+        instruments_list += inst + ",";
 
-	while(True):
-		try:
-			if IsForbiddenTime():
-				print "BOLSA CERRADA";
-				sleep(60);
-				continue;
+    while (True):
+        try:
+            if IsForbiddenTime():
+                print "BOLSA CERRADA";
+                sleep(60);
+                continue;
 
-			pricesStream = PricesStream();
+            pricesStream = PricesStream();
 
-			for tick in pricesStream.GetStreamingData(instruments_list):
-				if IsForbiddenTime():
-					print "BOLSA CERRADA";
-					sleep(60);
-					break;
+            for tick in pricesStream.GetStreamingData(instruments_list):
+                if IsForbiddenTime():
+                    print "BOLSA CERRADA";
+                    sleep(60);
+                    break;
 
-				instrument = tick['instrument']
-				price = tick['price']
+                instrument = tick['instrument']
+                price = tick['price']
 
-				instrument_part_A = instrument.split('_')[0];
-				instrument_part_A = instrument_part_A.encode('ascii','ignore');
-				instrument_part_B = instrument.split('_')[1];
-				instrument_part_B = instrument_part_B.encode('ascii','ignore');
+                instrument_part_A = instrument.split('_')[0];
+                instrument_part_A = instrument_part_A.encode('ascii', 'ignore');
+                instrument_part_B = instrument.split('_')[1];
+                instrument_part_B = instrument_part_B.encode('ascii', 'ignore');
 
-				instrument = instrument_part_A + '_' + instrument_part_B;
+                instrument = instrument_part_A + '_' + instrument_part_B;
 
-				if instrument == "EU50_EUR" or instrument == "DE30_EUR" or instrument == "FR40_EUR" or instrument == "NL25_EUR" or instrument == "XAU_EUR":
-					continue
+                if instrument == "EU50_EUR" or instrument == "DE30_EUR" or instrument == "FR40_EUR" or instrument == "NL25_EUR" or instrument == "XAU_EUR":
+                    continue
 
-				result = ProcessPrice(instrument, price);
-				if DEBUG:
-					if result == 1:
-						print instrument + ": " + "LONG"
-					elif result == -1:
-						print instrument + ": " + "SHORT"
-		except KeyboardInterrupt:
-			break;
-		except:
-			errorsManagement.captureException();
-			pass;
+                result = ProcessPrice(instrument, price);
+                if DEBUG:
+                    if result == 1:
+                        print instrument + ": " + "LONG"
+                    elif result == -1:
+                        print instrument + ": " + "SHORT"
+        except KeyboardInterrupt:
+            break;
+        except:
+            errorsManagement.captureException();
+            pass;
+
 
 def ProcessPrice(instrument, price):
-	check_result, entry_price, stop_loss_price, time = ichimoku.Verify(instrument)
-	if check_result is None:
-		return
+    check_result, entry_price, stop_loss_price, time = ichimoku.Verify(instrument)
+    if check_result is None:
+        return
 
-	#if apiData.GetMarginUsed() >= 400:
-	#	return
+    # if apiData.GetMarginUsed() >= 400:
+    #	return
 
-	if PutOrder(check_result, instrument, entry_price, price, stop_loss_price):
-		print time + ": " + str(entry_price) + "/" + str(stop_loss_price)
-		return check_result;
-	else:
-		return None;
+    if PutOrder(check_result, instrument, entry_price, price, stop_loss_price):
+        print time + ": " + str(entry_price) + "/" + str(stop_loss_price)
+        return check_result;
+    else:
+        return None;
+
 
 def PutOrder(order_type, instrument, entry_price, price, stop_loss):
-	could_put_order = False
+    could_put_order = False
 
-	if apiData.ExistsTradeOfInstrument(instrument) or apiData.ExistsOrderOfInstrument(instrument):
-		trades = apiData.GetTradesOpened()
-		instrument_trades = []
+    if apiData.ExistsTradeOfInstrument(instrument) or apiData.ExistsOrderOfInstrument(instrument):
+        trades = apiData.GetTradesOpened()
+        instrument_trades = []
 
-		for trade in trades:
-			if trade['instrument'] == instrument:
-				instrument_trades.append(trade)
+        for trade in trades:
+            if trade['instrument'] == instrument:
+                instrument_trades.append(trade)
 
-		sorted(instrument_trades, key=lambda x: datetime.strptime(x['openTime'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
+        sorted(instrument_trades, key=lambda x: datetime.strptime(x['openTime'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
 
-		last_trade = instrument_trades[0]
+        last_trade = instrument_trades[0]
 
-		partially_closed = False
-		if float(last_trade['initialUnits']) < 0:
-			partially_closed = float(last_trade['initialUnits']) < float(last_trade['currentUnits'])
-		else:
-			partially_closed = float(last_trade['initialUnits']) > float(last_trade['currentUnits'])
+        partially_closed = False
+        if float(last_trade['initialUnits']) < 0:
+            partially_closed = float(last_trade['initialUnits']) < float(last_trade['currentUnits'])
+        else:
+            partially_closed = float(last_trade['initialUnits']) > float(last_trade['currentUnits'])
 
-		if partially_closed:
-			could_put_order = True
-		else:
-			could_put_order = False
-	else:
-		could_put_order = True
+        if partially_closed:
+            print instrument + ": A GANADORA"
 
-	if could_put_order:
-		date = datetime.now()
+        if partially_closed:
+            could_put_order = True
+        else:
+            could_put_order = False
+    else:
+        could_put_order = True
 
-		balance = apiData.GetBalance()
+    if could_put_order:
+        date = datetime.now()
 
-		units = apiData.GetUnitsForPrice(balance * 0.01, instrument, instrumentsManager.instruments[instrument]['precision'], granularity, instrumentsManager.instruments[instrument]['rate']);
+        balance = apiData.GetBalance()
 
-		if units is None or units == 0:
-			print "Can't have units to " + instrument
-			return None;
+        units = apiData.GetUnitsForPrice(balance * 0.01, instrument,
+                                         instrumentsManager.instruments[instrument]['precision'], granularity,
+                                         instrumentsManager.instruments[instrument]['rate']);
 
-		order_id = str(uuid.uuid1())
+        if units is None or units == 0:
+            print "Can't have units to " + instrument
+            return None;
 
-		i, p, d = str(price).partition('.');
-		price_precision = len(d);
+        order_id = str(uuid.uuid1())
 
-		stop_loss = apiData.GetPriceFormatted(stop_loss, instrumentsManager.instruments[instrument]['pricePrecision']);
+        i, p, d = str(price).partition('.');
+        price_precision = len(d);
 
-		total_units = order_type * float(units);
+        stop_loss = apiData.GetPriceFormatted(stop_loss, instrumentsManager.instruments[instrument]['pricePrecision']);
 
-		entry_price = apiData.GetPriceFormatted(entry_price, instrumentsManager.instruments[instrument]['pricePrecision']);
+        total_units = order_type * float(units);
 
-		result = False;
+        entry_price = apiData.GetPriceFormatted(entry_price,
+                                                instrumentsManager.instruments[instrument]['pricePrecision']);
 
-		if order_type == 1:
-			if entry_price < price:
-				result = OrdersData().MakeMarketOrder(order_id, instrument, date, total_units, stop_loss);
-			else:
-				result = OrdersData().MakeLimitOrder(order_id, instrument, entry_price, date, total_units, stop_loss);
-		else:
-			if entry_price > price:
-				result = OrdersData().MakeMarketOrder(order_id, instrument, date, total_units, stop_loss);
-			else:
-				result = OrdersData().MakeLimitOrder(order_id, instrument, entry_price, date, total_units, stop_loss);
+        result = False;
 
-		if result == True:
-			print "Made " + instrument + " order with id " + order_id + " with " + str(order_type * units) + " units"
-			return True;
+        if order_type == 1:
+            if entry_price < price:
+                result = OrdersData().MakeMarketOrder(order_id, instrument, date, total_units, stop_loss);
+            else:
+                result = OrdersData().MakeLimitOrder(order_id, instrument, entry_price, date, total_units, stop_loss);
+        else:
+            if entry_price > price:
+                result = OrdersData().MakeMarketOrder(order_id, instrument, date, total_units, stop_loss);
+            else:
+                result = OrdersData().MakeLimitOrder(order_id, instrument, entry_price, date, total_units, stop_loss);
 
-		return False;
+        if result == True:
+            print "Made " + instrument + " order with id " + order_id + " with " + str(order_type * units) + " units"
+            return True;
+
+        return False;
+
 
 def IsForbiddenTime():
-	weekday = datetime.today().weekday();
-	today = datetime.today();
+    weekday = datetime.today().weekday();
+    today = datetime.today();
 
-	if weekday == 4 and today.hour > 21:
-		return True;
+    if weekday == 4 and today.hour > 21:
+        return True;
 
-	if weekday == 5:
-		return True;
+    if weekday == 5:
+        return True;
 
-	if weekday == 6 and today.hour < 21:
-		return True;
+    if weekday == 6 and today.hour < 21:
+        return True;
 
-	return False;
+    return False;
+
 
 if __name__ == "__main__":
     main()
