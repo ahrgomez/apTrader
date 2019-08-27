6252b780b8d232b48d14d0e7013cdbdfceaf783d
Alejandro Hernández Gómez <alejandro.hernandez@jscda.com>
2019-08-27 14:55:18 +0200

    get instruments by pip ranking

--- main.py
+++ main.py
@@ -70,9 +70,7 @@ def main():
         except KeyboardInterrupt:
             break;
         except Exception, e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno)
+            traceback.print_exc()
 
             errorsManagement.captureException();
             pass;
